6c4040536e9ba7ec90e8611af7b84094248ff769
fannil <fannil@users.noreply.github.com>
2018-03-28 10:55:09 +0200

    Add the Character with his basics inputs and a jump

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    public float maxSpeed = 10f;
+    private Rigidbody2D _rigidBody2D;
+    private bool isJumping = false;
+    // Use this for initialization
+    void Start () {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical")*maxSpeed;
+        //Debug.Log(Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Vertical")>0)
+            isJumping = true;
+
+        _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, translation);
+        if (_rigidBody2D.position.y > 1)
+            _rigidBody2D.position = new Vector2(_rigidBody2D.position.x, 1);
+        Debug.Log(_rigidBody2D.position.y);
+    }
+}
