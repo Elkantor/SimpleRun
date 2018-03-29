2c309b374a975e88b588bf83b7aba1e5c2d912d7
Elkantor <vic03@live.fr>
2018-03-29 16:06:21 +0200

    Add the generation of the level at infinity

--- Assets/Scripts/Core/LevelComponent.cs
+++ Assets/Scripts/Core/LevelComponent.cs
@@ -13,27 +13,88 @@ public class LevelComponent : MonoBehaviour {
     public float colorB;
     public float colorG;
     public float speedScrolling = 0.01f;
+    public GameObject previousChunk;
+    public Chunk[] chunksPossibilities;
+    float[] probabilities;
+    float cameraBoundXNegative;
 
-    public void LoadLevelComponent(float width, float height, float probability, float colorR, float colorG, float colorB){
+    public void LoadLevelComponent(
+        float width, 
+        float height, 
+        float probability, 
+        float colorR, 
+        float colorG, 
+        float colorB, 
+        GameObject previousChunk, 
+        Chunk[] chunksPossibilities
+    )
+    {
         this.width = width;
         this.height = height;
         this.probability = probability;
         this.colorR = colorR;
         this.colorG = colorG;
         this.colorB = colorB;
+        this.previousChunk = previousChunk;
+        this.chunksPossibilities = chunksPossibilities;
+        probabilities = new float[chunksPossibilities.Length];
+        probabilities[0] = chunksPossibilities[0].chunkProbability;
+        for (int i = 1; i < chunksPossibilities.Length; i++)
+        {
+            probabilities[i] = probabilities[i - 1] + chunksPossibilities[i].chunkProbability;
+        }
+        
+
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+        cameraBoundXNegative = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x;
+
+    }
 
     // Update is called once per frame
     void Update(){
         if (gameStarted){
-            Debug.Log(transform.position.x);
             transform.position = new Vector3(transform.position.x - speedScrolling, transform.position.y, transform.position.z);
+            if((transform.position.x + width/10) <= cameraBoundXNegative)
+            {
+                RegenerateChunk();
+            }
         }
     }
 
+    void SetChunkLevelComponent(Chunk chunk, Vector3 position){
+        
+        width = chunk.chunkWidth;
+        height = chunk.chunkHeight;
+        probability = chunk.chunkProbability;
+        colorR = chunk.chunkColorRed;
+        colorG = chunk.chunkColorGreen;
+        colorB = chunk.chunkColorBlue;
+
+        spriteRenderer.color = new Color(chunk.chunkColorRed, chunk.chunkColorGreen, chunk.chunkColorBlue);
+        transform.localScale = new Vector3(width, height, 1);
+        Debug.Log("New width:" + width + " |  New height: " + height);
+        // Set the position of this game object, to be next to the previous game object
+        float newX = previousChunk.transform.position.x + (previousChunk.transform.localScale.x/10) + (width/10);
+        float newY = height/10;
+        transform.position = new Vector3(newX, newY, 0.0f);
+        
+    }
+
+    private void RegenerateChunk(){
+        float newProbability = Random.Range(0.0f, 0.99f);
+        float previousProbability = 0;
+        for (int i = 0; i < probabilities.Length; i++)
+        {
+            if (newProbability >= previousProbability && newProbability <= probabilities[i])
+            {
+                SetChunkLevelComponent(chunksPossibilities[i], previousChunk.transform.position);
+                return;
+            }
+            previousProbability += probabilities[i];
+        }
+    }
 }
--- Assets/Scripts/Core/Main.cs
+++ Assets/Scripts/Core/Main.cs
@@ -5,24 +5,46 @@ using UnityEngine;
 public class Main : MonoBehaviour {
 
     List<GameObject> levelChunks = new List<GameObject>();
+
     float currentPositionX = 0;
     float currentPositionY = 0;
 
-	void Start () {
-		DontDestroyOnLoad(gameObject);
+    void Start() {
+        DontDestroyOnLoad(gameObject);
         currentPositionX = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x;
         InitializeMap();
-	}
-	
-	private void InitializeMap(){
-		LoaderJSON loaderChunks = new LoaderJSON();
-		Chunk[] chunks = loaderChunks.LoadGameData("Datas/Chunks.json");
-        GameObject chunk = CreateChunk(chunks[0]);
-        GameObject chunk2 = CreateChunk(chunks[1]);
-        StartGame();
     }
 
-    GameObject CreateChunk(Chunk chunk){
+    private void InitializeMap() {
+        LoaderJSON loaderChunks = new LoaderJSON();
+        Chunk[] chunks = loaderChunks.LoadGameData("Datas/Chunks.json");
+
+        float[] probabilities = new float[chunks.Length];
+
+        probabilities[0] = chunks[0].chunkProbability;
+        for (int i = 1; i < chunks.Length; i++) {
+            probabilities[i] = probabilities[i - 1] + chunks[i].chunkProbability;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            float probability = Random.Range(0.0f, 1.0f);
+            float previousProbability = 0.0f;
+            for (int j = 0; j < probabilities.Length; j++) {
+                if (probability > previousProbability && probability <= probabilities[j]) {
+                    if (i == 0){
+                        CreateChunk(chunks[j], null, chunks);
+                    }
+                    else{
+                        CreateChunk(chunks[j], levelChunks[i-1], chunks);
+                    }
+                }
+                previousProbability = probabilities[j];
+            }
+        }
+        levelChunks[0].GetComponent<LevelComponent>().previousChunk = levelChunks[levelChunks.Count - 1];
+    }
+
+    void CreateChunk(Chunk chunk, GameObject previousChunk, Chunk[] chunksPossibilities){
         GameObject chunkGameObject = new GameObject("Chunk" + levelChunks.Count);
         LevelComponent levelComponent = chunkGameObject.AddComponent<LevelComponent>();
 
@@ -32,7 +54,9 @@ public class Main : MonoBehaviour {
             chunk.chunkProbability,
             chunk.chunkColorRed,
             chunk.chunkColorGreen,
-            chunk.chunkColorBlue
+            chunk.chunkColorBlue,
+            previousChunk,
+            chunksPossibilities
         );
         
         GameObject templateGameObject = GameObject.Find("Ground");
@@ -48,32 +72,31 @@ public class Main : MonoBehaviour {
 
         // Set the position of the new game object
         float newX = this.currentPositionX + (levelComponent.width / 10);
-        float newY = this.currentPositionY + (levelComponent.height / 10);
-        chunkGameObject.transform.position = new Vector3(newX, newY, 0);
+        //float newY = this.currentPositionY + (levelComponent.height / 10);
+        float newY = levelComponent.height / 10;
+        chunkGameObject.transform.position = new Vector3(newX, newY, 0.0f);
 
 
         // Update the coordinates of the current game object created in the scene
         this.currentPositionX = currentPositionX + (levelComponent.width / 5);
-        this.currentPositionY = newY - (levelComponent.height / 10);
 
         // Update the scale of the new game object
-        chunkGameObject.transform.localScale = new Vector2(chunk.chunkWidth, chunk.chunkHeight);
-        levelChunks.Add(chunkGameObject);
+        chunkGameObject.transform.localScale = new Vector3(chunk.chunkWidth, chunk.chunkHeight, 1.0f);
 
-        return chunkGameObject;
+        levelChunks.Add(chunkGameObject);
 
     }
 
     void StartGame(){
-        Debug.Log(levelChunks.Count);
         foreach(GameObject chunk in levelChunks){
             LevelComponent levelComponentScript = chunk.GetComponent<LevelComponent>();
-            //levelComponentScript.speedScrolling = 0.01f;
             levelComponentScript.gameStarted = true;
         }
     }
 
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown("space")) {
+            StartGame();
+        }
+    }
 }
