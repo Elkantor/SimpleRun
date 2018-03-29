18b77207efeb8f7c364c6c862e67e978fccea6d2
rmexia <remondo86@hotmail.fr>
2018-03-29 10:08:48 +0200

    Starting game

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+
+    public static bool started;
+	// Use this for initialization
+	void Start () {
+        started = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            started = true;
+            Destroy(gameObject);
+        }
+
+    }
+}
